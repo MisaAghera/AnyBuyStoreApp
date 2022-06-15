469c343c6f0e0763b102368947f26c222aba5dfd
misa_esb <misa.aghera@esparkbizmail.com>
2022-06-15 10:34:24 +0530

    angular added home, sign up and login page

--- AnyBuyStore/Controllers/ProductWishController.cs
+++ AnyBuyStore/Controllers/ProductWishController.cs
@@ -1,4 +1,4 @@
-﻿using AnyBuyStore.Core.Handlers.ProductWish.Commands.AddProductWish;
+﻿//using AnyBuyStore.Core.Handlers.ProductWish.Commands.AddProductWish;
 using AnyBuyStore.Core.Handlers.ProductWish.Commands.DeleteProductWish;
 using AnyBuyStore.Core.Handlers.ProductWish.Queries.GetAllProductWishById;
 using Microsoft.AspNetCore.Mvc;
@@ -7,37 +7,37 @@ using AnyBuyStore.Core.Handlers.ProductCartHandler.Commands.UpdateProductQuantit
 namespace AnyBuyStore.Controllers
 {
    
-    public class ProductWishController : BaseApiController
-    {
-        public ProductWishController(ILogger<BaseApiController> logger, IMediator mediator) : base(logger, mediator)
-        {
-        }
+    //public class ProductWishController : BaseApiController
+    //{
+    //    public ProductWishController(ILogger<BaseApiController> logger, IMediator mediator) : base(logger, mediator)
+    //    {
+    //    }
 
-        [HttpGet]
-        public async Task<IActionResult> GetAll(int UserId, CancellationToken cancellationToken)
-        {
-            return Ok(await _mediator.Send(new GetAllProductWishByUserIdQuery { UserId = UserId }, cancellationToken));
-        }
+    //    [HttpGet]
+    //    public async Task<IActionResult> GetAll(int UserId, CancellationToken cancellationToken)
+    //    {
+    //        return Ok(await _mediator.Send(new GetAllProductWishByUserIdQuery { UserId = UserId }, cancellationToken));
+    //    }
 
-        [HttpPost]
-        public async Task<IActionResult> Add(AddProductwishCommand command, CancellationToken cancellationToken)
-        {
-            return Ok(await _mediator.Send(command, cancellationToken));
-        }
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> Delete(int id, CancellationToken cancellationToken)
-        {
-            return Ok(await _mediator.Send(new DeleteProductWishCommand { Id = id }, cancellationToken));
-        }
+    //    [HttpPost]
+    //    public async Task<IActionResult> Add(AddProductwishCommand command, CancellationToken cancellationToken)
+    //    {
+    //        return Ok(await _mediator.Send(command, cancellationToken));
+    //    }
+    //    [HttpDelete("{id}")]
+    //    public async Task<IActionResult> Delete(int id, CancellationToken cancellationToken)
+    //    {
+    //        return Ok(await _mediator.Send(new DeleteProductWishCommand { Id = id }, cancellationToken));
+    //    }
 
-        [HttpPut("{id}")]
-        public async Task<IActionResult> Update(int id, UpdateProductQuantityInCartCommand command, CancellationToken cancellationToken)
-        {
-            command.In.Id = id;
-            return Ok(await _mediator.Send(command, cancellationToken));
-        }
+    //    [HttpPut("{id}")]
+    //    public async Task<IActionResult> Update(int id, UpdateProductQuantityInCartCommand command, CancellationToken cancellationToken)
+    //    {
+    //        command.In.Id = id;
+    //        return Ok(await _mediator.Send(command, cancellationToken));
+    //    }
 
-    }
+    //}
 
 }
 
